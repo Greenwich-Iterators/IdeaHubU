1bc003e7c97cd3729ad49202e05852b94723c31e
PrymusLu <kamanga.elijah@gmail.com>
2024-11-04 20:37:11 +0200

    fix: change role type to string, then enforce enum using object.values method

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	role: { type: Roles, required: true }
+	role: { type: String, enum: Object.values(Roles), required: true }
 });
 
 userSchema.pre("save", async function (next) {
