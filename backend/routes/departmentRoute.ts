d25f37688757046c39b6c33fa93dd262d7153ece
PrymusLu <kamanga.elijah@gmail.com>
2024-11-08 09:51:53 +0200

    chore: add department router

---
+++ backend/routes/departmentRoute.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const departmentRouter = express.Router();
+
+departmentRouter.post("/add", async (req: Request, res: Response) => {
+	// try{
+	// 	const { name}
+	// }
+})
+
+export default departmentRouter
