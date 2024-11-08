2be980ec13f2824e200cfe5ab3c060db93653d1d
PrymusLu <kamanga.elijah@gmail.com>
2024-11-08 09:51:35 +0200

    chore: add comment router endpoints

---
+++ backend/routes/commentRouter.ts
@@ -0,0 +1,132 @@
+import express, {Request, Response} from "express";
+import Idea from "../models/ideasModel"
+import User from "../models/userModel"
+import Comment from "../models/commentModel"
+
+const commentRouter = express();
+
+//add a comment
+commentRouter.post("/add", async (req: Request, res: Response) => {
+	try {
+		const {ideaId, userId, content} = req.body;
+
+		if(!content){
+			res.status(400)
+				.json({
+					success:false,
+					error: "Comment content cannot be empty"
+				})
+			return;
+		}
+
+		const existingIdea = await Idea.findById(ideaId);
+		if(!existingIdea){
+			res.status(400)
+				.json({
+					success: false,
+					error: "Couldn't not find any idea matching the request."
+				});
+			return;
+		}
+
+		const existingUser = await User.findById(userId);
+		if(!existingUser){
+			res.status(400)
+				.json({
+					success: false,
+					error: "Couldn't not find any user matching the request."
+				})
+			return;
+		}
+
+		const newComment = new Comment({
+			ideaId: ideaId,
+			userId: userId,
+			content: content,
+			hidden: false
+		})
+
+		await Comment.create(newComment);
+
+		res.status(200)
+			.json({
+				success: true,
+				message: "Successfully added comment"
+			})
+
+	}
+	catch{
+
+	}
+})
+
+//edit a comment
+commentRouter.put("/edit", async (req: Request, res: Response) => {
+	const { commentId, content } = req.body;
+
+	if(!content){
+		res.status(400)
+			.json({
+				success: false,
+				error: "Comment cannot be empty."
+			})
+		return;
+	}
+
+	const existingComment = await Comment.findById(commentId);
+
+	if(!existingComment){
+		res.status(400)
+			.json({
+			success: false,
+			error: "Couldn't find requested comment."
+		})
+		return;
+	}
+
+	existingComment.content = content;
+
+	await Comment.updateOne({_id: commentId}, existingComment);
+
+	res.status(200)
+		.json({
+			success: true,
+			message: "Successfully edited comment"
+		})
+})
+
+//delete a comment
+commentRouter.delete("/delete", async (req: Request, res: Response) => {
+	const { commentID: commentId} = req.body;
+
+	const existingComment = await Comment.findById(commentId);
+
+	if(!existingComment){
+		res.status(404).json({
+			success: false,
+			error: "Couldn't to find requested comment"
+		})
+		return;
+	}
+
+	await Comment.deleteOne({_id: commentId});
+
+	res.status(200)
+		.json({
+			success: true,
+			message: "Successfully deleted comment"
+		})
+})
+
+//get all comments per idea
+commentRouter.get("/idea", async (req: Request, res: Response) => {
+	const {ideaId} = req.body;
+	const allIdeaComments = await Comment.find({ideaId: ideaId});
+
+	res.status(200).json({
+		success: true,
+		comments: allIdeaComments
+	})
+})
+
+export default commentRouter;
