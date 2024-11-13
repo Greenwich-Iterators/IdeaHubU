217c9ebfff1c0aa5d0a805aff34773eb4bca373a
celestusC3 <celestus.nyalugwe@gmail.com>
2024-11-13 17:12:42 +0200

    Added Role Field in the Users Table

--- frontend/manager_dashboard.php
+++ frontend/manager_dashboard.php
@@ -194,7 +194,7 @@ function removeCategory($id)
                 // Check if there are users in the array Coming from Niza
                 if (!empty($users)) {
                     echo '<table border="1" cellpadding="10">';
-                    echo '<tr><th>First Name</th><th>Last Name</th><th>Username</th><th>Email</th></tr>';
+                    echo '<tr><th>First Name</th><th>Last Name</th><th>Username</th><th>Email</th><th>Role</th></tr>';
 
                     // Loop through each user and display their information
                     foreach ($users as $user) {
